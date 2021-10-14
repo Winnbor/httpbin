786540a14261b5d1d905fd8b8c4561a6f163c65d
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-10-14 17:35:36 +0300

    Change rendering of request info

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,9 +1,52 @@
+import { useState } from 'react';
+import { MdExpandMore, MdExpandLess } from 'react-icons/md';
 import './InfoCard.scss';
 
 export default function InfoCard({ info }) {
+  const [headersVisible, setHeadersVisible] = useState(false);
+
+  const handleToggleHeaders = () => {
+    setHeadersVisible(!headersVisible);
+  };
+
+  const keyChecker = key => Object.keys(info)?.includes(key);
+
   return (
     <div className="InfoCard">
-      <p className="render">{JSON.stringify(info, null, '\t')}</p>
+      <p className="render">"args": {JSON.stringify(info?.args, null, '\t')}</p>
+      {keyChecker('data') && (
+        <p className="render">
+          "data": {JSON.stringify(info?.data, null, '\t')}
+        </p>
+      )}
+      {keyChecker('files') && (
+        <p className="render">
+          "files": {JSON.stringify(info?.files, null, '\t')}
+        </p>
+      )}
+      {keyChecker('form') && (
+        <p className="render">
+          "form": {JSON.stringify(info?.form, null, '\t')}
+        </p>
+      )}
+      <p className="render">
+        "headers":{' '}
+        {headersVisible && (
+          <span>{JSON.stringify(info?.headers, null, '\t')}</span>
+        )}
+        <button className="toggler-btn" onClick={handleToggleHeaders}>
+          {headersVisible ? <MdExpandLess /> : <MdExpandMore />}
+        </button>
+      </p>
+      {keyChecker('json') && (
+        <p className="render">
+          "json": {JSON.stringify(info?.json, null, '\t')}
+        </p>
+      )}
+      <p className="render">
+        "origin": {JSON.stringify(info?.origin, null, '\t')}
+      </p>
+      <p className="render">"url": {JSON.stringify(info?.url, null, '\t')}</p>
     </div>
   );
 }
